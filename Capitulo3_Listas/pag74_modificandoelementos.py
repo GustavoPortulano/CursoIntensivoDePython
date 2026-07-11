c595cc292ba53225564ffbd42aba3abcaa966baf
GustavoPortulano <gfa.portulano@gmail.com>
2026-07-11 00:32:07 -0300

    Modificando elementos da lista

---
+++ Capitulo3_Listas/pag74_modificandoelementos.py
@@ -0,0 +1,9 @@
+"""Modificando elementos de uma lista"""
+
+print("\n")
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+
+print("Modificando elemento")
+motorcycles[0] = "ducati"
+print(motorcycles)
