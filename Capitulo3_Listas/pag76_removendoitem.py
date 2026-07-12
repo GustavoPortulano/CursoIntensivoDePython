450f85ba878dc242388edecc0c53f0df91f4aa80
GustavoPortulano <gfa.portulano@gmail.com>
2026-07-12 00:11:04 -0300

    Primeito commit do dia 12 de julho de 2021

--- Capitulo3_Listas/pag76_removendoitem.py
+++ Capitulo3_Listas/pag76_removendoitem.py
@@ -1,1 +1,10 @@
-print("26 de julho de 2026. Sábdo, noite fria.")
+print("\n12 de julho de 2026. Sábdo, noite fria.")
+print("Pagina 76 - Removendo um item usando a instrução del")
+
+motorcycles = ['honda','yamaha','suzuki']
+print(motorcycles)
+print("Eliminando o primeiro elemento:")
+del motorcycles[0]
+print(motorcycles)
+
+print("\n")
