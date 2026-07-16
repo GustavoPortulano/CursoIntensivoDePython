2deed4940c857c5996ea1ae25eaf4e20f5786bc1
GustavoPortulano <gfa.portulano@gmail.com>
2026-07-16 01:41:15 -0300

    Início da instrução IF

---
+++ Capitulo5_InstrucoesIf/pag110cars.py
@@ -0,0 +1,13 @@
+print("\n16 de julho de 2026. Quinta feira. Madrugada fria.")
+print("Capítulo 5 - Instruções If")
+print("Um exemplo simples.")
+
+print("\n")
+
+cars = ['audi','bmw','subaru','toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+print("\n")
