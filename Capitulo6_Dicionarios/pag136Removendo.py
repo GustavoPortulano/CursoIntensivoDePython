a204bbcb4cfc79a99a0853b991ad8c8a3842300f
GustavoPortulano <gfa.portulano@gmail.com>
2026-07-25 23:31:02 -0300

    Removendo com DEL e dicionairo de objetos semelhantes

---
+++ Capitulo6_Dicionarios/pag136Removendo.py
@@ -0,0 +1,10 @@
+print("\n25 de julho de 2026. Sábado. Noite fria.")
+print("Página 136 - Removendo pares chave-valor.")
+
+alien_0 = {'color':'green','points':5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
+
+print("\n")
---
+++ Capitulo6_Dicionarios/pag136aObjetosDiferentes.py
@@ -0,0 +1,10 @@
+print("\n25 de julho de 2026. Sábado. Noite fria.")
+print("Página 136 - Um dicionário de objetos diferentes.")
+
+favorite_languages = {'jen':'python','sarah':'c',
+                          'edward':'ruby','phil':'python',}
+
+print("Sarah's favorite language is " + 
+      favorite_languages['sarah'].title() + ".")
+
+print("\n")
