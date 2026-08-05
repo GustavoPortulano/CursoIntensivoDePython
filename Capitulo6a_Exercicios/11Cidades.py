7a46c1cb43023edfed66553987747a2c6f6307a7
GustavoPortulano <gfa.portulano@gmail.com>
2026-08-05 00:45:58 -0300

    Exercícios 9 e 11(por fazer)

--- Capitulo6a_Exercicios/11Cidades.py
+++ Capitulo6a_Exercicios/11Cidades.py
@@ -5,3 +5,10 @@ print("6.11 - Cidades: Crie um dicionário chamado cities. Use os nomes de três
 "\naproxixmada e um fato sobre essa cidade. As chaves do dicionário de cada cidade" \
 "\ne todas as informações como country, population e fact. Apresente o nome de cada" \
 "\ncidade e toas as informações que você armazenou sobre ela.")
+
+print("\n")
+
+cities = {'isntabul':{'country':'turquia','population':5000000,'fact':'antiga capital bizantina',},
+          'rio de janeiro':{'country':'brasil','population':65000000,'fact':'antiga capital imperial do brasil'},
+          'jerusalem':{'country':'israel','population':2300000,'fact':'disputada por três religiões'}}
+          
--- Capitulo6a_Exercicios/9LugaresFavoritos.py
+++ Capitulo6a_Exercicios/9LugaresFavoritos.py
@@ -4,10 +4,20 @@ print("6.9 - Lugares favoritos: Crie um dicionário chamado favorite_places. Pen
 "\nlugares favoritos para cada pessoa. Percorra o dicionário com um laço e apresente" \
 "\n o nome de cada pessoa e seus lugares favoritos.\n")
 
-favorite_places = {'vilma':['praia','sítio',],
+favorite_places = {'vilma':['praia','sítio'],
                    'beth':['shopping','praia','clube'],
                    'gean':['clube','estádio','sitio']}
 
+"""Nomeando a chave como NOME  e o valor como LUGARES, o dicionário fica dividido
+em duas partes, que são trabalhadas de forma independente em cada laço FOR."""
+
+for nome, lugares in favorite_places.items():
+    print(f"Os lugares favoritos de {nome.title()} são:")
+    for lugar in lugares:
+        print("\t" + f"*{lugar}")
+
+print("\n")
+    
 
 
     
