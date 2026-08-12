15f2d5e854a94467fc3b3106a83301e636757ff5
GustavoPortulano <gfa.portulano@gmail.com>
2026-08-11 22:17:47 -0300

    Exemplos das páginas 169 e 170

---
+++ Capitulo8_Funcoes/pag169GreetUser.py
@@ -0,0 +1,10 @@
+print("\n11 de agosto de 2026. Terça-feira. Noite fria.")
+print("Página 169 - Definindo uma função.")
+
+"""Uso da palavra reservada def para definir uma função."""
+def greet_user():
+    """Exibe uma saudação simples."""
+    print("Hello!")
+
+greet_user()
+print("\n")
---
+++ Capitulo8_Funcoes/pag170GreetUser.py
@@ -0,0 +1,9 @@
+print("\n11 de agosto de 2026. Terça-feira. Noite fria.")
+print("Página 170 - Passando informações para uma função.")
+
+def greet_user(username):
+    """Exibe uma saudação simples."""
+    print("Hello, " + username.title() + "!")
+
+greet_user('jesse')
+greet_user('sarah')
